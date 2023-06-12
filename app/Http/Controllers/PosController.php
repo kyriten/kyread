101d0a511737e648892042e7c39529acb244a377
asnyuuki <78668567+ossascyber@users.noreply.github.com>
2023-06-12 18:39:59 +0700

    update pos controller

--- app/Http/Controllers/PosController.php
+++ app/Http/Controllers/PosController.php
@@ -3,101 +3,76 @@
 namespace App\Http\Controllers;
 
 use App\Models\Pos;
-use App\Http\Requests\StorePosRequest;
-use App\Http\Requests\UpdatePosRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class PosController extends Controller
 {
-    // public function index()
-    // {
-    //     return view('pos.access.index',[
-    //         'title' => 'Login',
-    //         'active' => 'access'
-    //     ]);
-    // }
-    // public function addshop(Request $request)
-    // {
-    //     $validatedData = $request->validate([
-    //         'username' => 'required|min:1|max:12|unique:users',
-    //         'email' => 'required|email:dns|unique:users',
-    //         'password' => 'required|confirmed|min:8|max:16',
-    //         'password_confirmation' => 'required|min:8|max:16',
-    //     ]);
-
-    //     $validatedData['password_confirmation'] = Hash::make($validatedData['password_confirmation']);
-
-    //     User::create($validatedData);
-    //     $request->session()->flash('success', 'Registration successfull! Please login.');
-    //     return redirect('/');
-    // }
     /**
      * Display a listing of the resource.
      */
-    public function dashboard()
+    public function dashboard(Pos $pos)
     {
-        return view('pos.pages.dashboard',[
-            'title' => 'Dashboard',
-        ]);
+        $pos = Pos::all();
+        return view('pos.pages.dashboard', compact('pos'));
     }
 
     public function products()
     {
-        return view('pos.pages.products',[
+        return view('pos.pages.products', [
             'title' => 'Product',
         ]);
     }
 
     public function addproduct()
     {
-        return view('pos.pages.addproduct',[
+        return view('pos.pages.addproduct', [
             'title' => 'Add Product',
         ]);
     }
 
     public function editproduct()
     {
-        return view('pos.pages.editproduct',[
+        return view('pos.pages.editproduct', [
             'title' => 'Edit Product',
         ]);
     }
 
-    public function stall()
+    public function storeprofile()
     {
-        return view('pos.pages.stall',[
-            'title' => 'Store Profile',
-        ]);
+        $pos = Pos::all();
+        return view('pos.pages.storeprofile', compact('pos'));
     }
 
     public function transaction()
     {
-        return view('pos.pages.transaction',[
+        return view('pos.pages.transaction', [
             'title' => 'Store Profile',
         ]);
     }
 
     public function locked()
     {
-        return view('pos.pages.featurelocked',[
+        return view('pos.pages.featurelocked', [
             'title' => 'Feature is Locked',
         ]);
     }
 
     public function logoutshop()
     {
-        return redirect ('/home');
+        return redirect('/home');
     }
 
     public function faq()
     {
-        return view('pos.pages.faq',[
+        return view('pos.pages.faq', [
             'title' => 'F.A.Q',
         ]);
     }
 
     public function contact()
     {
-        return view('pos.pages.contact',[
+        return view('pos.pages.contact', [
             'title' => 'Contact Us',
         ]);
     }
@@ -105,19 +80,67 @@ class PosController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function createRegister()
     {
-        //
+        return view('pos.access.register', [
+            'title' => 'Register',
+            'active' => 'access'
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StorePosRequest $request)
+    public function store(Request $posrequest)
     {
-        //
+        $validatedData = $posrequest->validate([
+            'name' => 'required|unique:pos',
+            'email' => 'required|email:dns|unique:pos',
+            'description' => 'required',
+            'password' => 'required|min:8|max:16',
+        ]);
+
+        Pos::create($validatedData);
+        $request->session()->flash('success', 'Registration successfull! Welcome.');
+        return redirect('/dashboard');
     }
 
+    public function createLogin()
+    {
+        return view('pos.access.login', [
+            'title' => 'Login',
+            'active' => 'access'
+        ]);
+    }
+    public function shoplogout()
+    {
+        Auth::logout();
+
+        request()->session()->invalidate();
+
+        request()->session()->regenerateToken();
+
+        return redirect('/home');
+    }
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'login_store_name' => 'required',
+            'login_store_password' => 'required'
+        ]);
+        $credentials['name'] = $credentials['login_store_name'];
+        $credentials['password'] = $credentials['login_store_password'];
+        unset($credentials['login_store_name']);
+        unset($credentials['login_store_password']);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/dashboard');
+        }
+
+        return back()->with('loginError', 'Login failed!');
+    }
     /**
      * Display the specified resource.
      */
@@ -137,7 +160,7 @@ class PosController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePosRequest $request, Pos $pos)
+    public function update(Request $request, Pos $pos)
     {
         //
     }
