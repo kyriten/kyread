c186dee7dc2206ba7b568f071daddb2866ee9c6a
asnyuuki <78668567+ossascyber@users.noreply.github.com>
2023-06-12 18:40:45 +0700

    update Product model

---
+++ app/Models/Product.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+
+
+class Product extends Model
+{
+    use HasFactory, Sluggable;
+
+    protected $guarded = ['id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+    
+}
