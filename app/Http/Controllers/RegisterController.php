5ede198bc02d5acab12636cbe97860f9fd0ece5d
asnyuuki <78668567+ossascyber@users.noreply.github.com>
2023-06-12 18:40:14 +0700

    update register controller

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -10,7 +10,7 @@ class RegisterController extends Controller
 {
     public function index()
     {
-        return view('access.index',[
+        return view('access.index', [
             'title' => 'Register',
             'active' => 'access'
         ]);
